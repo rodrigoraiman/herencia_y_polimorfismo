7df07c971211279af016f1f6c13961d1f7b1fbed
rodrigoraiman <rodrigoraiman@gmail.com>
2022-01-04 19:10:32 -0300

    ajustes

--- ejercicio2.rb
+++ ejercicio2.rb
@@ -1,85 +1,106 @@
 module Habilidades
-    module Volador
-      def volar
-        'Estoy volandooooo!'
-      end
-      def aterrizar
-        'Estoy cansado de volar, voy a aterrizar'
-      end
+  module Volador
+    def volar
+      "Estoy volandooooo!"
     end
-    module Nadador
-      def nadar
-        'Estoy nadando!'
-      end
-      def sumergir
-        'glu glub glub glu'
-      end
+    def aterrizar
+      "Estoy cansado de volar, voy a aterrizar"
     end
-    module Caminante
-      def caminar
-        'Puedo caminar!'
-      end
+  end
+  module Nadador
+    def nadar
+        "Estoy nadando!"
     end
-   end
-   module Alimentacion
-    module Herbivoro
-      def comer
-        'Puedo comer plantas!'
-      end
+     def sumergir
+      "glu glub glub glu"
     end
-    module Carnivoro
-      def comer
-        'Puedo comer carne!'
-    
-      end
+  end
+  module Caminante
+    def caminar
+      "Puedo caminar!"
     end
-   end
+  end
+end
 
-   class Animal
-        attr_reader :nombre
-        def initialize(nombre)
-        @nombre = nombre
-        end
+module Alimentacion
+  module Herbivoro
+    def comer
+      "Puedo comer plantas!"
     end
+  end
+  module Carnivoro
+    def comer
+      "Puedo comer carne!"
+    end
+  end
+end  
+class Animal
+  attr_reader :nombre
+  def initialize(nombre)
+    @nombre = nombre
+  end
+end  
+class Ave < Animal
+end  
+   
+class Mamifero < Animal
+end  
+   
+class Insecto < Animal
+end  
+   
+class Pinguino < Ave
+   include Habilidades::Nadador
+   include Habilidades::Caminante
+   include Alimentacion::Carnivoro
+end  
+class Paloma < Ave
+    include Habilidades::Nadador
+    include Habilidades::Caminante
+    include Alimentacion::Carnivoro
+end
 
-    class Ave < Animal
-    end
+class Pato < Ave
+   include Habilidades::Volador
+   include Habilidades::Caminante
+   include Habilidades::Nadador
+   include Alimentacion::Herbivoro
+   include Alimentacion::Carnivoro
+end  
 
-    class Mamifero < Animal
-    end
+class Perro < Mamifero
+    include Habilidades::Caminante
+    include Habilidades::Nadador
+    include Alimentacion::Carnivoro
+end
 
-    class Insecto < Animal
-    end
+   
+class Gato < Mamifero
+   include Habilidades::Caminante
+   include Alimentacion::Carnivoro
+end  
+  
+class Vaca < Mamifero
+   include Habilidades::Caminante
+   include Alimentacion::Herbivoro
+end 
 
-    class Pinguino < Ave
-        include Habilidades::Nadador
-        include Habilidades::Caminante
-        include Alimentacion::Carnivoro
-    end
+class Mosca < Insecto
+  include Habilidades::Volador
+  include Alimentacion::Carnivoro
+  include Alimentacion::Herbivoro
 
-    class Pato < Ave
-        include Habilidades::Volador
-        include Habilidades::Caminante
-        include Habilidades::Nadador
-        include Alimentacion::Herbivoro
-        include Alimentacion::Carnivoro
-    end
+end 
 
-    class Gato < Mamifero
-        include Habilidades::Caminante
-        include Alimentacion::Carnivoro
-    end
+class Mariposa < Insecto
+   include Habilidades::Volador
+   include Alimentacion::Herbivoro
+end 
 
-    class Vaca < Mamifero
-        include Habilidades::Caminante
-        include Alimentacion::Herbivoro
-    end
-
-    class Mariposa < Insecto
-        include Habilidades::Volador
-        include Habilidades::Caminante
-        include Alimentacion::Herbivoro
-    end
+class Abeja < Insecto
+    include Habilidades::Volador
+    include Alimentacion::Herbivoro
+end
 
 
 
